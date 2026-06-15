01a272b14f5b57815bacb8732cdf5e5508b30381
JP Duminy <jp@duminy.org>
2026-06-15 13:02:09 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
+"""
+
+Package installation.
+Controls the outputs of pip commands.
+
+"""
+
 from setuptools import setup, find_packages
 
 setup(
-  name='octavian',
-  version=0.1,
-  url='https://github.com/jszpila314/octavian',
-  author='Jakub Szpila',
-  author_email='jakub.szpila314@gmail.com',
+  name="octavian",
+  version="0.2.1",
+  url="https://github.com/jp-duminy/octavian",
+  author="JP Duminy, Jakub Szpila",
+  maintainer="JP Duminy",
+  author_email="jp@duminy.org",
   packages=find_packages(),
-  python_requires='>=3.11',
+  python_requires='>=3.13',
   install_requires=[
-    'numpy', 'pandas', 'scipy', 'astropy', 'unyt', 'h5py'
-  ]
+    "numpy", "pandas", "scipy", "astropy", "unyt", "h5py",
+    "joblib", "numba", 
+  ],
+  extras_require={"MPI": ["mpi4py",]},
+  description="Simulation analysis toolkit for SPH codes."
 )
