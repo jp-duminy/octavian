ffcfcdcc634fa32b943248e2ce39d5f542a2cd5d
JP Duminy <jp@duminy.org>
2026-07-16 12:06:38 +0100

    Added HBT HERONS parsing methods (not tested against real catalogue for lack of possessing one). Read from SOAP/toolbox implementation to understand file layouts and output structures.

--- octavian/external_halo_sources/hbt-herons.py
+++ octavian/external_halo_sources/hbt-herons.py
@@ -11,9 +11,139 @@ HBT algorithm source paper: https://academic.oup.com/mnras/article/474/1/604/456
 
 """
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from octavian.data_management import SnapshotReader
+
 import h5py
 import numpy as np
 from pathlib import Path
+from functools import (
+    cached_property,
+)  # for avoiding rereading files across methods but also not holding too much in __init__
+
+from .halos import (
+    HaloAssignments,
+    SubhaloInformation,
+    HaloSource,
+    build_contiguous_id_lookup,
+)
+
+
+class HeronsHaloSource(HaloSource):
+    """
+    HBT-HERONS halo ID source; only compatible with SWIFT, and requires SWIFT halo FOF groups to be identified.
+    """
+
+    def __init__(self, catalogue_dir: Path, snap_nr: int, reader: SnapshotReader) -> None:
+
+        self.catalogue_path = resolve_subsnap_paths(catalogue_dir=catalogue_dir, snap_nr=snap_nr)
+        self.reader = reader
+
+    @cached_property
+    def _properties(self) -> tuple[np.ndarray, ...]:
+        """
+        All subsnap file properties.
+        """
+        return read_subsnap_properties(catalogue_path=self.catalogue_path)
+
+    @cached_property
+    def hid_lookup(self) -> np.ndarray:
+        """
+        Returns global HostHaloID lookup array.
+        """
+        _, host_halo_ids, _ = self._properties
+        return build_contiguous_id_lookup(
+            ids=host_halo_ids
+        )  # need a global lookup; not all hids are necessarily found in each ptype
+
+    @cached_property
+    def subhid_lookup(self) -> np.ndarray:
+        """
+        Returns global TrackID lookup array.
+        """
+        track_ids, _, _ = self._properties
+        return build_contiguous_id_lookup(ids=track_ids)
+
+    def read_halo_ids(self, ptypes: list[str]) -> HaloAssignments:
+        """
+        Returns a HaloAssignments dataclass containing the assignments made by HERONS.
+        """
+        herons_pids, offsets = read_subsnap_particles(catalogue_path=self.catalogue_path)
+        order = np.argsort(
+            herons_pids, stable=True
+        )  # this is best precomputed otherwise you argsort the pid array 4 times in the merge
+        sorted_herons_pids = herons_pids[order]
+
+        track_ids, host_halo_ids, _ = self._properties
+
+        halo_assignments, subhalo_assignments = {}, {}
+
+        for (
+            ptype
+        ) in ptypes:  # to sort the ids we first need all ptypes present to cover all HaloIDs to avoid desync issues
+            snapshot_pids = self.reader.read_particle_ids(ptype=ptype)
+
+            halo_ids, subhalo_ids = match_particle_ids(
+                snapshot_pids=snapshot_pids,
+                sorted_herons_pids=sorted_herons_pids,
+                order=order,
+                particle_offsets=offsets,
+                track_ids=track_ids,
+                host_halo_ids=host_halo_ids,
+            )
+
+            halo_assignments[ptype] = _apply_lookup(
+                ids=halo_ids, lookup=self.hid_lookup
+            )  # handles unmatched particles' sentinels
+            subhalo_assignments[ptype] = _apply_lookup(ids=subhalo_ids, lookup=self.subhid_lookup)
+
+        return HaloAssignments(halo_ids=halo_assignments, subhalo_ids=subhalo_assignments)
+
+    def read_subhalo_info(self) -> SubhaloInformation:
+        """
+        Reads the HERONS subhalo information.
+        """
+        track_ids, host_halo_ids, n_bound = self._properties
+        return SubhaloInformation(
+            parent_ids=_apply_lookup(ids=host_halo_ids, lookup=self.hid_lookup),
+            track_ids=track_ids,
+            n_bound=n_bound,
+        )  # this is basically a wrapper; I envision AHF needing more on this method
+
+
+def match_particle_ids(
+    snapshot_pids: np.ndarray,
+    sorted_herons_pids: np.ndarray,
+    order: np.ndarray,
+    particle_offsets: np.ndarray,
+    track_ids: np.ndarray,
+    host_halo_ids: np.ndarray,
+) -> tuple[np.ndarray, ...]:
+    """
+    Matches the raw snapshot PIDs with the HERONS-assigned IDs, returning a tuple of particle (HaloIDs, SubhaloIDs) aligned with the raw snapshot PIDs.
+    """
+    insertion_idx = np.searchsorted(sorted_herons_pids, snapshot_pids)
+    insertion_idx = np.minimum(
+        insertion_idx, len(sorted_herons_pids) - 1
+    )  # prevent OOB index when max(snapshot_pids) > max(herons_pids)
+    matched = sorted_herons_pids[insertion_idx] == snapshot_pids
+
+    aligned_hids = np.full(shape=len(snapshot_pids), fill_value=-1, dtype=np.int64)
+    aligned_subhids = np.full(shape=len(snapshot_pids), fill_value=-1, dtype=np.int64)
+
+    herons_idx = order[
+        insertion_idx[matched]
+    ]  # RHS fancy indexing maps matched, which is sorted, to unsorted id arrays
+    subhalo_idx = (
+        np.searchsorted(particle_offsets, herons_idx, side="right") - 1
+    )  # use side=right to match [offsets[i]:offsets[i+1]]; -1 for the subhalo itself
+
+    aligned_hids[matched] = host_halo_ids[subhalo_idx]
+    aligned_subhids[matched] = track_ids[subhalo_idx]
+
+    return aligned_hids, aligned_subhids
 
 
 def read_subsnap_particles(catalogue_path: Path) -> tuple[np.ndarray, ...]:
@@ -60,3 +190,13 @@ def resolve_subsnap_paths(catalogue_dir: Path, snap_nr: int) -> Path:
         raise FileNotFoundError(f"{matches} output catalogues found in {catalogue_dir}, please check the directory.")
 
     return matches[0]
+
+
+def _apply_lookup(ids: np.ndarray, lookup: np.ndarray) -> np.ndarray:
+    """
+    Small helper to use the global id lookup with the ptype id arrays.
+    """
+    result = np.full_like(ids, fill_value=-1)
+    matched = ids != -1  # there will be unmatched particles and [-1] on an ndarray picks last element, so must mask
+    result[matched] = lookup[ids[matched]]
+    return result
