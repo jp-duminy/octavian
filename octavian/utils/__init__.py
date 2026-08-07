ee9b406dbd3358cee4a44cfcfcc8aba0f93d30f4
JP Duminy <jp@duminy.org>
2026-08-07 17:05:09 +0100

    Added front-facing API.

--- octavian/utils/__init__.py
+++ octavian/utils/__init__.py
@@ -1,3 +1,11 @@
 from .loader import (
     load as load,
 )
+from .generate_snapshots import (
+    generate_gizmo_snapshot as generate_gizmo_snapshot,
+    generate_swift_snapshot as generate_swift_snapshot,
+)
+from .toolbox import (
+    generate_test_catalogue as generate_test_catalogue,
+    repack_catalogue as repack_catalogue,
+)
