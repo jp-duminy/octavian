4d47bd2bb90df63e1e83203a22d6a707ab0d98ed
JP Duminy <jp@duminy.org>
2026-08-07 16:04:55 +0100

    Updated documentation and changed unlink logic so we don't delete good files.

--- octavian/utils/toolbox.py
+++ octavian/utils/toolbox.py
@@ -20,16 +20,16 @@ def repack_catalogue(
     compression_level: int = 4,  #  https://en.wikipedia.org/wiki/Gzip fastest is 4
 ) -> None:
     """
-    Repack an HDF5 catalogue with GZIP compression in-place. The compressed catalogue is named the same as the original.
+    Calls h5repack with with subprocess to apply gzip compression to a catalogue. The compressed catalogue is named the same as the original.
 
     Parameters
     ----------
     catalogue_path : Path
         Path to the catalogue you would like to compress.
     compression_level : int
-        Desired GZIP compression level (1-9)
+        Desired gzip compression level (1-9)
     """
-    logger.info(f"Compressing catalogue to GZIP-level {compression_level}.")
+    logger.info(f"Compressing catalogue to gzip compression level {compression_level}.")
     h5repack_binary = shutil.which("h5repack")
     if h5repack_binary is None:
         raise FileNotFoundError("h5repack is not available on your system, to recompress files please install it.")
@@ -43,5 +43,6 @@ def repack_catalogue(
             check=True,
         )
         temp_path.replace(catalogue_path)
-    finally:  # if h5repack fails, delete the file
+    except Exception:  # if h5repack fails, delete the corrupted file
         temp_path.unlink(missing_ok=True)
+        raise
