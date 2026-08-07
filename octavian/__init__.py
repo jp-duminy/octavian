0dcf03bf2ba64b814f9c7252aa5548a518e6daf0
JP Duminy <jp@duminy.org>
2026-08-07 17:05:09 +0100

    Added front-facing API.

---
+++ octavian/__init__.py
@@ -0,0 +1,22 @@
+from .data_management import OctavianConfig as OctavianConfig, OctavianConstants as OctavianConstants
+from .run_octavian import (
+    run_octavian as run_octavian,
+)
+from .utils import (
+    load as load,
+    generate_test_catalogue as generate_test_catalogue,
+    generate_swift_snapshot as generate_swift_snapshot,
+    generate_gizmo_snapshot as generate_gizmo_snapshot,
+    repack_catalogue as repack_catalogue,
+)
+
+__all__ = [
+    "OctavianConfig",
+    "OctavianConstants",
+    "run_octavian",
+    "load",
+    "generate_test_catalogue",
+    "generate_swift_snapshot",
+    "generate_gizmo_snapshot",
+    "repack_catalogue",
+]
