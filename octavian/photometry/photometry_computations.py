f865aed5909042129198c5796f080d5145f2f215
JP Duminy <jp@duminy.org>
2026-08-14 17:12:03 +0100

    Added file for photometry engine room.

---
+++ octavian/photometry/photometry_computations.py
@@ -0,0 +1,5 @@
+"""
+
+The photometry engine room. Photometry has a less well-defined boundary than aggregate_properties between simple helpers and heavyweight physics computations, but for the most part, these functions are the ones which dominate runtime and are called by the run_photometry function.
+
+"""
