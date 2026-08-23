1390ff3dd59ba4f54b5cb13c6b1087b7f6996bd1
JP Duminy <jp@duminy.org>
2026-08-23 23:00:30 +0500

    Added import testing.

---
+++ tests/test_imports.py
@@ -0,0 +1,33 @@
+"""
+
+Tests external and internal imports.
+
+"""
+
+
+def test_external_dependencies() -> None:
+    """
+    Checks whether all external dependencies are importable.
+    """
+    # the big five
+    import numpy as np
+    import numba
+    import h5py
+    import astropy.units as u
+    import mpi4py
+
+    # random operations so ruff does not delete the imports
+    assert np.array([1, 2, 3]).sum() == 6
+    assert numba.njit(lambda x: x + 1)(1) == 2
+    assert u.kpc.to(u.cm) > 0
+    assert h5py.string_dtype() is not None
+    assert mpi4py.MPI.COMM_WORLD.Get_size() >= 1
+
+
+def test_package_imports() -> None:
+    """
+    Tests whether internal imports work.
+    """
+    import octavian
+
+    assert hasattr(octavian, "load")  # this will fail if the load function gets renamed
